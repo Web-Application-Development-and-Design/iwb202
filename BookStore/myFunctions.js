473a1c5d6dbaeb748738f5bad8889f8ab90974a7
Munir Ahmad <munirna6891@gmail.com>
2024-11-20 16:13:34 -0800

    First new project
    upload first project all files and images

---
+++ BookStore/myFunctions.js
@@ -0,0 +1,297 @@
+const properties = [
+  {
+    id: 0,
+    price: 500000,
+    name_book: "  ما بين الهوى والهاوية   ",
+    ISBN_book: '978-9770916741',
+    details: `
+          <p>تاريخ النشر: 12/11/2024</P>
+          <p> الناشر: خاص - أمل محمد عطار</p>
+          <p>النوع: كتاب إلكتروني </p>
+          <p>نبذة الناشر: بعضاً من الهوى حب.. وبعضاً منه ميلات.. في هذه السطور تتالت كل الميلات بحب</p>
+    `,
+    image: "images/41511.jpg",
+  },
+  {
+    id: 1,
+    price: 250000,
+    name_book: "ما وراء الصدفه ؛ رحلة إلى قلب الغرض الكوني",
+    ISBN_book: '178-9770916823',
+    details: `
+              <p>تاريخ النشر: 15/11/2024</p>
+              <p>الناشر: خاص - هادي هانز نخله</p>
+              <p>النوع: كتاب إلكتروني</p>
+              <p>توفر الكتاب: يتوفر في غضون 48 ساعة</p>
+              <p>نبذة الناشر:
+                " ما وراء الصدفه"" هو كتاب يأخذ القراء في رحلة عميقة لاستكشاف أحد أكثر الأسئلة أهمية وإلحاحًا في الحياة:
+                ما هو الغرض من وجودنا؟ من خلال عدسة الفلسفة، والعلم، والروحانية، والخبرة الإنسانية،
+                يغوص هذا الكتاب في فكرة أنه لا توجد صدفة حقيقية في الكون - وأن كل لحظة وكل لقاء مشبع بمعنى أعمق، حتى لو لم يكن ذلك واضحًا على الفور.
+                </p>
+    `,
+    image: "images/411077.jpg",
+  },
+  {
+    id: 2,
+    price: 600000,
+    name_book: "في بيتنا فيزياء",
+    ISBN_book: '578-9770916954',
+    details: `   
+              <p>تاريخ النشر: 11/05/2020</p>
+              <p>الناشر: بيان للترجمة والنشر والتوزيع</p>
+              <p>ينصح لأعمار بين: 9-12 سنوات</p>
+              <p>النوع: كتاب إلكتروني</p>
+              <p>نبذة الناشر:
+          كتاب موجه للأطفال من 6 – 15 سنة ولآبائهم ليراقبوا معهم العديد من التجارب الفيزيائية. ومعظم هذه التجارب سهلة، وغير مكلفة. 
+          ومثلما قام المؤلف بعمل التجارب في البيت، بإمكانك الوالدين القيام بها مع طفلهم في المنزل، بل وأكثر.
+          ويثير الكتاب عند الطفل عدة أسئلة مثل: ما هي الفيزياء
+           وما معناها؟ لماذا نرى بعضنا البعض؟ لماذا نسمع بعضنا البعض؟
+          لماذا نمشي على الأرض، ولا نمشي على الماء؟
+          لماذا السحاب أبيض، والسماء زرقاء؟</p>
+    `,
+    image: "images/251286.jpg",
+  },
+  {
+    id: 3,
+    price: 900000,
+    name_book: "امتداح الفاشنيستا",
+    ISBN_book: '678-9770917096',
+    details: `
+                  <p>تاريخ النشر: 28/11/2017</p>
+                  <p>الناشر: ثقافة للنشر والتوزيع</p>
+                  <p>أنواع اخرى: ورقي غلاف عادي </p>
+                  <p>نبذة الناشر:
+ الفاشنيستا» العمل الروائي الجديد للكاتب حسين البار (ثقافة للنشر والتوزيع، 2017)، يتحدث خلاله عن  نحو
+ الشهرة عبر وسائل الإعلام المختلفة،
+  ولا سيما شبكات التواصل الاجتماعي ويفعل هذا من خلال تسليط الضوء على  فتاة (موديل) تعيش عالماً افتراضياً صنعته بنفسها.</p>
+    `,
+    image: "images/298547.jpg",
+  },
+  {
+    id: 4,
+    price: 900000,
+    name_book: "الرجل الرفيع بالقميص الأبيض",
+    ISBN_book: '378-9770917248',
+    details: `
+                <p>تاريخ النشر: 14/10/2024</p>
+                <p>دار ممدوح عدوان للنشر والتوزيع</p>
+                <p>النوع: ورقي غلاف عادي</p>
+                <p>نبذة الناشر:
+          "أين اختفى شاكر الصافي؟ تتكاثر التكهّنات والاحتمالات، فيما هو قابعٌ في مكانٍ مجهول يحادث الصمت والوحدة والخوف،
+          وهو الذي أمضى حياته هامشياً، غير مرئيّ تقريباً، لم يتدخّل في شيء، ولا سعى يوماً إلى شيء.
+          من هذا الحدث ينطلق ""ممدوح عزام"" ليروي عن المدينة في زمن الفوضى والجنون،
+          لكنّه لا يكتفي بتعدّد أصوات الرواة، مدخلاً للسرد، بقدر ما يسمح لكلّ واحدٍ منهم أن يغضب، ويناقض نفسه
+          والآخرين، مقترحاً بذلك زاويةً جديدة للنظر في ما حدث ويحدث.</p>
+    `,
+    image: "images/711392.jpg",
+  },
+  {
+    id: 5,
+    price: 120000,
+    name_book: "أسرار صوفيا",
+    ISBN_book: '478-9770917357',
+    details: `
+                  <p>تاريخ النشر: 23/09/2024</p>
+                  <p>الناشر: دار ملهمون للنشر والتوزيع</p>
+                  <p>النوع: ورقي غلاف عادي</p>
+                  <p>نبذة الناشر:
+            تدور أحداث هذه الرواية في جزيرة كاستيلوريزو على ثلاثة أجيال. القصة مبنية على 
+            أحداث تاريخية حقيقية حصلت في اليونان من زلزال عام 1926 وما تلاه بعدها من أحداث 
+            وصولًا إلى الحرب العالمية الثانية وكيف أثرت هذه الحرب على هذه الجزيرة المنسية.</p>
+                  
+      
+    `,
+    image: "images/710522.jpg",
+  },
+  {
+    id: 6,
+    price: 900000,
+    name_book: "كف بلا أصابع",
+    ISBN_book: '278-9770917469',
+    details: `
+              <p>تاريخ النشر: 23/09/2024</p>
+              <p>الناشر: أوراق للنشر والتوزيع</p>
+              <p>النوع: ورقي غلاف عادي</p>
+              <p>نبذة الناشر:
+كانت أمي تعشق الزنبق الأبيض , حديقة بيتنا الصغيرة أشبه بالحلم الذي يمر أمامي
+ , أقاصيص الزنبق من كل جانب , أريكة قديمة بنية اللون , و هذه الطاولة التي تراها و مقاعد خشبية قديمة
+ , والكثير من الحنين , أفتقدهما , أنا عاجز و ضعيف أمام ذاكرتي الملطخة بالسواد , أنا غريب هنا , ابتعدت و تبت إلى الله , لكن الناس لم تتركني , من يعلم حكايتي من أهل الحي , يساومني على الماضي من جديد , و أنا لا أتقن إلا الهرب .. الهرب</p>
+    `,
+    image: "images/298604.jpg",
+  },
+  {
+    id: 7,
+    price: 300000,
+    name_book: "عمة آل مشرق",
+    ISBN_book: '321-9770917794',
+    details: `
+              <p>تاريخ النشر: 15/10/2024</p>
+              <p>الناشر: دار الآداب</p>
+              <p>النوع: ورقي غلاف عادي</p>
+              <p>تتدهور حالةُ الجازي الصحية في الرياض، فيواجِه والدُها خياريَن: إمّا الشّروع في حفر قبرٍ لها، \
+              وإمّا القَبول بعرض الممرّض الأميركي ماثيو إيدن لاصطحابها إلى مستشفى الإرسالية في البحرين.
+              مرغماً، يوافق على ترحيل ابنتِه للعلاج شرطَ أن يتزوّجها ماثيو ويعتنقَ الإسلام مهراً لها</P>
+    `,
+    image: "images/410272.jpg",
+  },
+  {
+    id: 8,
+    price: 200000,
+    name_book: "نادية في مغامرة ساحرة",
+    ISBN_book: '357-9770917580',
+    details: `
+              <p>تاريخ النشر: 01/01/2024</p>
+              <p>الناشر: هاشيت أنطوان</p>
+              <p>ينصح لأعمار بين: 9-12 سنوات</p>
+              <p>النوع: ورقي غلاف عادي</p>
+              <p>نادية شابّةٌ تهوى جمع الدُّمى ذات الرؤوس النطّاطة وتُخبّئ في جعبتها ""معلومة عالماشي"" لكلّ مناسبةٍ وحدثٍ وتدافع بجدٍّ عن تاريخ حضارتها...
+               المصريّة. في الواقع، عندما كانت نادية طفلةً، هاجرت عائلتها من مصر إلى أميركا حيث تعيش اليوم محاطةً برفاقها، شلّة الدّحّاحين</p>
+    `,
+    image: "images/397040.jpg",
+  },
+  {
+    id: 9,
+    price: 1900000,
+    name_book: "يختبئ في ظلي",
+    ISBN_book: '256-9770917632',
+    details: `
+              <p>تاريخ النشر: 25/07/2014</p>
+              <p> الناشر: الدار العربية للعلوم ناشرون</p>
+              <p>النوع: كتاب إلكتروني/epub (جميع الأنواع)</p>
+              <p>نبذة الناشر:
+نصوص مليكة عبد الحميد، في "يختبئ في ظلّي" عابرة كالريح، أفكارها تلامس سطح الأشياء من دون أن تتوغل في العمق، وهو مكمن روعتها. تحقن الشاعرة نصها بحمولات ومعاني أقرب إلى أن تكون جزءاً من سيرة حياة عابرة في الأمكنة والأزمنة، فالكلمات هنا تأتي بصفتها تعبيراً عن "الذات" التي تكتمل فرديتها
+ حينما تواجه العالم وحيدة، هذا المصير يتجسد في النص إصغاءً عميقاً من قبل الشاعرة لذاتها،
+ ولِما هو خفيّ ويعز على التوصيف في تجربة مرور الإنسان بالعالم والوجود،
+  وقد انسلت بنفس سردي ملحمي وإحساس رفيع بقيمة الإنسان/ الفرد ونظرته إلى ذاته وإلى الوجود</p>
+    `,
+    image: "images/231627.jpg",
+  },
+ 
+];
+
+const createPropertiesTable = () => {
+  const propertiesRowsContainer = document.querySelector(".properties .body");
+/**
+ * اختيار نوع الازرار أظهار التفاصيل و اختيار
+ */
+  properties.forEach((property) => {
+    propertiesRowsContainer.innerHTML += `
+        <div class="row">
+            <div class="col">${property.ISBN_book}</div>
+            <div class="col">${property.name_book}</div>
+            <div class="col">${property.price} SYP</div>
+            <div class="col">
+                <input type="checkbox" class="col-but details-but">
+            </div>
+            <div class="col">
+            <center>
+                <input type="radio" name="color" value="red"  class="col-but but-select" >
+                </center>
+            </div>
+        </div>
+        <div class="details-row">
+            <div class="details-wrapper">
+                <h2>تفاصيل الكتاب</h2>
+                <div class="details">${property.details}</div>
+                <img src="${property.image}" alt="Property image" />
+            </div>
+        </div>
+    `;
+  });
+
+  const buttons = document.querySelectorAll(".details-but");
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      const detailsRow = document.querySelectorAll(".details-row")[index];
+      detailsRow.classList.toggle("show");
+    });
+  });
+};
+
+const selectProperty = () => {
+  const continuebutton = document.querySelector(".but-continue");
+  const buttons = document.querySelectorAll(".but-select");
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      const selectedProperty = properties[index];
+      if (button.classList.contains("selected")) {
+        button.classList.remove("selected");
+        continuebutton.disabled = true;
+        return;
+      }
+      buttons.forEach((button) => {
+        button.classList.remove("selected");
+      });
+      button.classList.add("selected");
+      // disable continue button
+      continuebutton.disabled = false;
+      console.log(selectedProperty);
+    });
+  });
+};
+
+const modalHandler = () => {
+  const continuebutton = document.querySelector(".but-continue");
+  const modal = document.querySelector(".modal");
+  const closeButton = document.querySelector(".close-but");
+  continuebutton.addEventListener("click", () => {
+    // check if it's not disabled
+    if (continuebutton.disabled) {
+      return;
+    }
+    modal.classList.add("show");
+  });
+  closeButton.addEventListener("click", () => {
+    modal.classList.remove("show");
+  });
+};
+
+function arabicOnly(e) {
+  // https://stackoverflow.com/questions/10364737/validation-on-arabic-characters
+  console.log(e);
+  var unicode = e.charCode ? e.charCode : e.keyCode;
+  if (unicode != 8 && unicode != 32) {
+    //if the key isn't the backspace key (which we should allow)
+    if (
+      (unicode < 48 || unicode > 57) &&
+      (unicode < 0x0600 || unicode > 0x06ff)
+    )
+      //if not a number or arabic
+      e.preventDefault(); //disable key press
+    return false; //disable key press
+  }
+}
+
+const formHandler = () => {
+  const form = document.querySelector("form");
+  const button = document.querySelector(".form-but");
+  const name = document.querySelector("#name");
+  const id = document.querySelector("#id");
+
+  name.addEventListener("keypress", arabicOnly);
+  id.addEventListener("keypress", (e) => {
+    if (e.charCode < 48 || e.charCode > 57) {
+      e.preventDefault();
+    }
+  });
+  id.addEventListener("change", (e) => {
+    // check if the id is 11 digits
+    if (e.target.value.length !== 11) {
+      e.target.classList.add("invalid");
+      e.target.setCustomValidity("الرجاء إدخال رقم الهوية بشكل صحيح");
+    } else {
+      e.target.setCustomValidity("");
+    }
+  });
+  button.addEventListener("click", () => {
+    // check if the form is valid
+    if (!form.checkValidity()) {
+      return;
+    }
+    form.submit();
+  });
+};
+
+createPropertiesTable();
+selectProperty();
+modalHandler();
+formHandler();
